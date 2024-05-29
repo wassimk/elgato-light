af080a8270294acc80d4112bfed488e372bb3530
Wassim Metallaoui <wassim@metallaoui.com>
2024-05-29 18:31:55 -0500

    feat: a CLI tool to turn an elgato ring light on and off
    My Elgato Ring Light is rarely detected by the Elgato Control Central
    software. I'm at my wits end trying to figure out why. I've been through
    it all. I even made a separate 2.4Ghz wireless network for it without
    success.
    
    During troubleshooting I learned it always has an IP address and never
    loses network connection. That's what led me to try and find it's
    interface. So, here we are. I going to control it with this CLI tool via
    Raycast on macOS.

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+use elgato_keylight::KeyLight;
+use std::error::Error;
+use std::net::Ipv4Addr;
+use std::str::FromStr;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "keylight")]
+enum KeyLightCli {
+    On,
+    Off,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let args = KeyLightCli::from_args();
+
+    let ip = Ipv4Addr::from_str("192.168.0.16")?;
+    let mut kl = KeyLight::new_from_ip("Key Light Air", ip, None).await?;
+
+    match args {
+        KeyLightCli::On => {
+            kl.set_power(true).await?;
+            kl.set_brightness(10).await?;
+            kl.set_temperature(3000).await?;
+        }
+        KeyLightCli::Off => {
+            kl.set_power(false).await?;
+        }
+    }
+
+    let status = kl.get().await?;
+    println!("{:?}", status);
+    Ok(())
+}

6064b7416f9646e8cc7cc233b5c0cc3e5978cc1f
Wassim Metallaoui <wassim@metallaoui.com>
2024-05-29 18:46:56 -0500

    feat: command line arguments for ip address, temperature and brightness

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,16 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "keylight")]
 enum KeyLightCli {
-    On,
+    On {
+        #[structopt(long = "brightness", default_value = "10")]
+        brightness: u8,
+
+        #[structopt(long = "temperature", default_value = "3000")]
+        temperature: u32,
+
+        #[structopt(long = "ip-address", default_value = "192.168.0.16")]
+        ip_address: String,
+    },
     Off,
 }
 
@@ -15,21 +24,32 @@ enum KeyLightCli {
 async fn main() -> Result<(), Box<dyn Error>> {
     let args = KeyLightCli::from_args();
 
-    let ip = Ipv4Addr::from_str("192.168.0.16")?;
-    let mut kl = KeyLight::new_from_ip("Ring Light", ip, None).await?;
-
     match args {
-        KeyLightCli::On => {
+        KeyLightCli::On {
+            brightness,
+            temperature,
+            ip_address,
+        } => {
+            let ip = Ipv4Addr::from_str(&ip_address)?;
+            let mut kl = KeyLight::new_from_ip("Ring Light", ip, None).await?;
+
             kl.set_power(true).await?;
-            kl.set_brightness(10).await?;
-            kl.set_temperature(3000).await?;
+            kl.set_brightness(brightness).await?;
+            kl.set_temperature(temperature).await?;
+
+            let status = kl.get().await?;
+            println!("{:?}", status);
         }
         KeyLightCli::Off => {
+            let ip = Ipv4Addr::from_str("192.168.0.16")?;
+            let mut kl = KeyLight::new_from_ip("Ring Light", ip, None).await?;
+
             kl.set_power(false).await?;
+
+            let status = kl.get().await?;
+            println!("{:?}", status);
         }
     }
 
-    let status = kl.get().await?;
-    println!("{:?}", status);
     Ok(())
 }

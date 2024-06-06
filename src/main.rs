6d3a249b0237cac8ec11ea7b84bdf456ebde8646
Wassim Metallaoui <wassim@metallaoui.com>
2024-06-05 22:05:15 -0500

    refactor: another round of de-duplication

--- src/main.rs
+++ src/main.rs
@@ -43,52 +43,45 @@ enum KeyLightCli {
     },
 }
 
-async fn create_keylight(ip_address: String) -> Result<KeyLight, Box<dyn Error>> {
-    let ip = Ipv4Addr::from_str(&ip_address)
-        .map_err(|e| format!("Failed to parse IP address: {}", e))?;
-    let kl = KeyLight::new_from_ip("Ring Light", ip, None)
-        .await
-        .map_err(|e| format!("Failed to create KeyLight: {}", e))?;
-    Ok(kl)
+async fn get_keylight(ip_address: String) -> Result<KeyLight, Box<dyn Error>> {
+    let ip_address = Ipv4Addr::from_str(&ip_address)?;
+    let keylight = KeyLight::new_from_ip("Ring Light", ip_address, None).await?;
+    Ok(keylight)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args = KeyLightCli::from_args();
+    let ip_address = match args {
+        KeyLightCli::On { ref ip_address, .. } => ip_address.clone(),
+        KeyLightCli::Off { ref ip_address } => ip_address.clone(),
+        KeyLightCli::Brightness { ref ip_address, .. } => ip_address.clone(),
+        KeyLightCli::Temperature { ref ip_address, .. } => ip_address.clone(),
+    };
+
+    let mut keylight = get_keylight(ip_address).await?;
 
     match args {
         KeyLightCli::On {
             brightness,
             temperature,
-            ip_address,
+            ..
         } => {
-            let mut kl = create_keylight(ip_address).await?;
-
-            kl.set_power(true).await?;
-            kl.set_brightness(brightness).await?;
-            kl.set_temperature(temperature).await?;
+            keylight.set_power(true).await?;
+            keylight.set_brightness(brightness).await?;
+            keylight.set_temperature(temperature).await?;
         }
-        KeyLightCli::Off { ip_address } => {
-            let mut kl = create_keylight(ip_address).await?;
-
-            kl.set_power(false).await?;
+        KeyLightCli::Off { .. } => {
+            keylight.set_power(false).await?;
         }
-        KeyLightCli::Brightness {
-            brightness,
-            ip_address,
-        } => {
-            let mut kl = create_keylight(ip_address).await?;
+        KeyLightCli::Brightness { brightness, .. } => {
             let relative_brightness = brightness as f64 / 100.0;
-
-            kl.set_relative_brightness(relative_brightness).await?;
+            keylight
+                .set_relative_brightness(relative_brightness)
+                .await?;
         }
-        KeyLightCli::Temperature {
-            temperature,
-            ip_address,
-        } => {
-            let mut kl = create_keylight(ip_address).await?;
-
-            kl.set_temperature(temperature).await?;
+        KeyLightCli::Temperature { temperature, .. } => {
+            keylight.set_temperature(temperature).await?;
         }
     }
 
